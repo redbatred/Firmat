01cafe0a0c95f59b4c3a6377201490c31938d48b
redbatred <donredjon@gmail.com>
2024-05-26 22:14:25 +0200

    Qa bot

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-import header from './header/header.jsx'
+import Header from './header/Header.jsx'
+
+function App () {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h2>Welcome to my React app!</h2>
+        {/* Other components and content can go here */}
+      </main>
+    </div>
+  );
+}
 
 
 export default App
